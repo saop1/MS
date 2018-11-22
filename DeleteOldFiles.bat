9fe63c980c1bfc4b764d70fd42d46ca3590903b5
saop1 <45061190+saop1@users.noreply.github.com>
2018-11-22 14:08:55 +0100

    Update DeleteOldFiles.bat

--- DeleteOldFiles.bat
+++ DeleteOldFiles.bat
@@ -1,3 +1,2 @@
-REM Delete Old Files
-REM Change all CAPS lines to ur thing and thangs
-forfiles /p "LOCATION" /s /m *.* /c "cmd /c Del @path" /d -AMOUNT OF DAYS OLD FILES TO REMOVE
+REM Change LOCATION to the location and -40 to how old files you want removed (example: -40 days)
+forfiles /p "LOCATION" /s /m *.* /c "cmd /c Del @path" /d -40
