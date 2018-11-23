5fa8365cf7f70dc91cc35b18a8926abd2e3b8639
saop1 <45061190+saop1@users.noreply.github.com>
2018-11-23 09:37:37 +0100

    Update DeleteOldFiles.bat

--- DeleteOldFiles.bat
+++ DeleteOldFiles.bat
@@ -1,3 +1,3 @@
 REM Change LOCATION to the location and -40 to how old files you want removed (example: -40 days)
-REM Use this as a task sequence
+REM Use this script with task sequencer
 forfiles /p "LOCATION" /s /m *.* /c "cmd /c Del @path" /d -40
